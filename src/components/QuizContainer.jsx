bee82af4f7202e40c8a7a6297871f0f73306defe
psych0wh1z <ikeagudike@gmail.com>
2024-09-28 15:51:31 +0100

    Fix bugs in component state rendering

--- src/components/QuizContainer.jsx
+++ src/components/QuizContainer.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Button from "./Button";
 import QuizQuestion from "./QuizQuestion";
-import useQuiz from "../hooks/useQuiz";
+import ScoreDisplay from "./ScoreDisplay";
 import questionSet from "../assets/questions"
 
 export default function QuizContainer() {
@@ -13,6 +13,26 @@ export default function QuizContainer() {
   const startQuiz = () => {
     setQuizStarted(true);
   }
+
+  const decrementIndex = (questionIndex) => {
+    setQuestionIndex(questionIndex - 1);
+  }
+
+  const incrementIndex = (questionIndex) => {
+    setQuestionIndex(questionIndex + 1);
+  }
+
+  const updateQuizAnswers = (selectedOption, questionIndex) => {
+    const updatedAnswers = [...quizAnswers];
+
+    updatedAnswers[questionIndex] = {
+      questionIndex,
+      selectedOption,
+      correctOption: questionSet[questionIndex].answer,
+    };
+
+    setQuizAnswers(updatedAnswers);
+  };
 
   const endQuiz = () => {
     let score = 0;
@@ -28,60 +48,64 @@ export default function QuizContainer() {
   }
 
 
-  const incrementIndex = (questionIndex) => {
-    setQuestionIndex(questionIndex + 1);
-  }
-
-  const decrementIndex = (questionIndex) => {
-    setQuestionIndex(questionIndex - 1);
-  }
-
-  const updateQuizAnswers = (selectedOption, questionIndex) => {
-    const updatedAnswers = [...quizAnswers];
-
-    updatedAnswers[questionIndex] = {
-      questionIndex,
-      selectedOption,
-      correctOption: questionSet[questionIndex].answer,
-    };
-
-    setQuizAnswers(updatedAnswers);
-  };
-
-
   return (
     <>
-      <h1 className="text-teal-500">Solidity Quiz</h1>
+      <h1 className="text-4xl font-bold mb-8 p-2.5 shadow-lg text-teal-500">Solidity Quiz</h1>
 
       {!!quizStarted
-        ? <QuizQuestion questionIndex={questionIndex} handleSelect={updateQuizAnswers} />
+        ? <QuizQuestion
+          questionIndex={questionIndex}
+          handleSelect={updateQuizAnswers}
+          selectedOption={quizAnswers[questionIndex]?.selectedOption} />
         : ""}
 
-      {!quizStarted && questionIndex === 0
-        ? <Button className="button" check={quizStarted} handleClick={startQuiz}>START</Button>
-        : ""}
+      {!quizStarted && questionIndex === questionSet.length - 1
+        ? (
+          <ScoreDisplay
+            score={playerScore}
+            totalQuestions={questionSet.length}
+            onRestart={() => {
+              setQuestionIndex(0);
+              setQuizStarted(true);
+              setQuizAnswers([]);
+              setPlayerScore(0);
+            }}
+          />)
+        : null}
 
-      {/* {!!quizStarted && questionIndex > 0 ? <Button className="button" index={questionIndex} handleClick={decrementIndex}>Previous</Button> : ""}
-      {!!quizStarted && questionIndex < 6 ? <Button className="button" index={questionIndex} handleClick={incrementIndex}>Next</Button> : ""} */}
+      <div className="flex flex-row justify-evenly content-center mt-3">
+        {!quizStarted && questionIndex === 0
+          ? <Button
+            className="button mt-5 text-teal-400"
+            check={quizStarted}
+            handleClick={startQuiz}>START</Button>
+          : ""}
 
-      {!!quizStarted && questionIndex > 0 && (
-        <Button className="button" index={questionIndex} handleClick={decrementIndex}>
-          Previous
-        </Button>
-      )}
+        {!!quizStarted && questionIndex > 0 && (
+          <Button
+            className="button mt-5"
+            index={questionIndex}
+            handleClick={decrementIndex}>
+            Previous
+          </Button>
+        )}
 
-      {!!quizStarted && questionIndex < questionSet.length - 1 && (
-        <Button className="button" index={questionIndex} handleClick={incrementIndex}>
-          Next
-        </Button>
-      )}
+        {!!quizStarted && questionIndex < questionSet.length - 1 && (
+          <Button
+            className="button mt-5 text-teal-400"
+            index={questionIndex}
+            handleClick={incrementIndex}>
+            Next
+          </Button>
+        )}
 
-      {!!quizStarted && questionIndex === 6
-        ? <Button className="button" check={quizStarted} handleClick={endQuiz}>SUBMIT</Button >
-        : ""}
-
-      <p>{questionIndex}</p>
-      <p>{playerScore}</p>
+        {!!quizStarted && questionIndex === 6
+          ? <Button
+            className="button mt-5 text-teal-400"
+            check={quizStarted}
+            handleClick={endQuiz}>SUBMIT</Button >
+          : ""}
+      </div>
     </>
   )
 }
--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -1,20 +1,20 @@
-import questionSet from "../assets/questions"
+import questionSet from "../assets/questions";
 
-export default function QuizQuestion({ questionIndex, handleSelect }) {
+export default function QuizQuestion({ questionIndex, handleSelect, selectedOption }) {
   const currentQuestion = questionSet[questionIndex];
 
   return (
     <>
-      <div>
+      <div className="bg-white text-teal-600 p-6 rounded-lg shadow-md mb-4">
         {currentQuestion.question}
       </div>
 
-      <div className="choices-container">
+      <div className="choices-container flex flex-col space-y-2 mt-4">
         {currentQuestion.choices.map((choice, index) => (
           <button
-            className="py-2 px-4 rounded-md bg-teal-500 text-white hover:bg-teal-600 transition-colors choice-button"
             key={index}
-            // className="choice-button"
+            className={`py-2 rounded-md shadow transition duration-300 transform focus:outline-none 
+              ${selectedOption === index ? 'bg-teal-700 text-white' : 'bg-teal-500 text-white hover:bg-teal-600'}`}
             onClick={() => handleSelect(index, questionIndex)}>
             {choice}
           </button>
